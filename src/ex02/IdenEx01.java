ea5cc05d0eb0b2d4e76111fe8bcfb28aae3c80e7
SEONGHOON KIM <chugue85@gmail.com>
2023-12-13 12:40:21 +0900

    static

---
+++ src/ex02/IdenEx01.java
@@ -0,0 +1,20 @@
+package ex02;
+
+/**
+ *  변수 작성법
+ *  1. 카멜(낙타)표기법을 사용
+ *  2. 숫자를 먼저 사용하지 않는다.
+ *  3. 특수문자를 사용하지 않는다.
+ */
+public class IdenEx01 {
+    public static void main(String[] args) {
+        //int public = 1  키워드는 변수로 사용할 수 없다.
+        int abc__; // 절대 사용안함!!
+        int user_money; // 절대 사용안함!!
+        int userMoney; // 낙타 표기법
+        // int abc#;  -- 특수 문자 사용하지 말자.
+        int method;
+        double abs$$; // 특수문자 쓰지 마세요, 헷갈리니까
+        // int 1stMoney; -- 숫자로 변수로 시작할 수 없다.
+    }
+}
---
+++ src/ex04/MemEx01.java
@@ -0,0 +1,13 @@
+package ex04;
+
+class Person1 {    // class는 즉, 여러 자료형을 품고 있는 person은 custom 자료형이라 생각하면 된다.
+    static int age = 20;
+    static char gender = '여';    // int,char는 primitive 자료형
+}
+
+public class MemEx01 {
+    public void main(String[] args) {
+        System.out.println(Person1.age);
+
+    }
+}
