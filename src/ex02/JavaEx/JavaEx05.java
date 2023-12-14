d4e2b558c27b7e5e60bb5abe39455765b6254b5d
AI_Nomads <chugue85@gmail.com>
2023-12-15 00:13:58 +0900

    예제 수정

--- src/ex02/JavaEx/JavaEx05.java
+++ src/ex02/JavaEx/JavaEx05.java
@@ -8,9 +8,10 @@ public class JavaEx05 {
         int distance = 2100;
 
         Scanner sc = new Scanner(System.in);
+        System.out.print("소리를 듣는데 걸린 시간을 입력하세요 : ");
         int second = sc.nextInt();
 
         int lightning = (distance / soundSpeed) * second;
-        System.out.println("번개로부터의 거리는 : " + lightning);
+        System.out.println("번개로부터의 거리는 : " + lightning + "km");
     }
 }
--- src/ex02/JavaEx/JavaEx06.java
+++ src/ex02/JavaEx/JavaEx06.java
@@ -1,12 +1,23 @@
 package ex02.JavaEx;
 
+import java.util.Scanner;
+
 public class JavaEx06 {
     public static void main(String[] args) {
-        char drvName = 'c';
-        String drtName = "test";
-        String fName = "sample";
-        String ext = "py";
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println(drvName + ":\\" + drtName + "\\" + fName + "." + ext );
+        System.out.print("드라이브 명을 입력하세요 (c): ");
+        String drvName = sc.next();
+
+        System.out.print("디렉토리 이름을 입력하세요 (\\test\\): ");
+        String drtName = sc.next();
+
+        System.out.print("파일 이름을 입력하세요 (sample): ");
+        String fName = sc.next();
+
+        System.out.print("파일 확장자를 입력하세요 (py): ");
+        String ext = sc.next();
+
+        System.out.println(drvName +":"+ drtName + fName + "." + ext );
     }
 }
