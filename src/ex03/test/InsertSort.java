969667d4926526f144b8d22b75b411c899d91635
AI_Nomads <chugue85@gmail.com>
2023-12-19 08:59:02 +0900

    change

--- src/ex03/test/InsertSort.java
+++ src/ex03/test/InsertSort.java
@@ -2,64 +2,24 @@ package ex03.test;
 
 public class InsertSort {
     public static void main(String[] args) {
-        int[] a = {8, 5, 6, 2, 4};
-        int temp;
+        int[] arr = {8, 5, 6, 2, 4, 7, 19, 20, 1};
+        int a = arr.length;
+        int temp = 0;
 
-//        if (a[i] < a[i-1]){
-//            temp = a[i-1];
-//            a[i] = a[i-1];
-//            a[i-1] = temp;
-//        }
-
-        if (a[2] < a[1]){
-            temp = a[1];
-            a[1] = a[2];
-            a[2] = temp;
+        for (int i = 1; i < a; i++) {
+            for (int j = 0; j < a; j++) {
+                int z = i - j - 1;
+                if (z < 0) {
+                    break;
+                }
+                if (arr[i - j] < arr[z]) {
+                    temp = arr[i - j];
+                    arr[i - j] = arr[z];
+                    arr[z] = temp;
+                }
+            }
         }
-        if (a[1] < a[0]){
-            temp = a[0];
-            a[0] = a[1];
-            a[1] = temp;
-        }
-
-        if (a[3] < a[2]){
-            temp = a[2];
-            a[2] = a[3];
-            a[3] = temp;
-        }
-        if (a[2] < a[1]){
-            temp = a[1];
-            a[1] = a[2];
-            a[2] = temp;
-        }
-        if (a[1] < a[0]){
-            temp = a[0];
-            a[0] = a[1];
-            a[1] = temp;
-        }
-
-        if (a[4] < a[3]){
-            temp = a[3];
-            a[3] = a[4];
-            a[4] = temp;
-        }
-        if (a[3] < a[2]){
-            temp = a[2];
-            a[2] = a[3];
-            a[3] = temp;
-        }
-        if (a[2] < a[1]){
-            temp = a[1];
-            a[1] = a[2];
-            a[2] = temp;
-        }
-        if (a[1] < a[0]){
-            temp = a[0];
-            a[0] = a[1];
-            a[1] = temp;
-        }
-
-        for (int q : a){
+        for (int q : arr) {
             System.out.print(q + " ");
         }
     }
