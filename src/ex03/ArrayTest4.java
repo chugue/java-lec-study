a9369cfc5525848c46345cc2cc761c27169499fd
AI_Nomads <chugue85@gmail.com>
2023-12-20 11:08:33 +0900

    class grammar

--- src/ex03/ArrayTest4.java
+++ src/ex03/ArrayTest4.java
@@ -1,6 +1,7 @@
 package ex03;
 
 public class ArrayTest4 {
+
     public static void main(String[] args) {
         int [] numbers = { 10, 20, 30 };
 //        System.out.println(numbers);  // numbers를 출력하면 그 배열의 주소값만 받을수 있다.
--- src/ex04/test/ScopeEx01_01.java
+++ src/ex04/test/ScopeEx01_01.java
@@ -1,26 +1,24 @@
 package ex04.test;
 
-public class ScopeEx01 {
+public class ScopeEx01_01 {
 
     int n1 = 1;
     static int n2 = 2;
 
     static void m1(){
-        int n5 = 10;  // static 변수 무색
+        int n5 = 10;
         System.out.println("m1 : " + n5);
     }
 
     void m2() {
-        System.out.println();
         System.out.println("m2 : " + n1);
     }
 
-    // 메인 시작 전 static에 n2변수와 m1메서드가 로드 되어 있음.
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println(1);
         m1();
         System.out.println(2);
-        ScopeEx01 sc = new ScopeEx01();
+        ScopeEx01_01 sc = new ScopeEx01_01();
         System.out.println(sc.n1);
         sc.m2();
     }
