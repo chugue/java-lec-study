75245cfa372a426c38ec757a241d0c11f25757ef
AI_Nomads <chugue85@gmail.com>
2023-12-20 11:42:47 +0900

    modified

--- src/ex03/GugudanEx04.java
+++ src/ex03/GugudanEx04.java
@@ -1,13 +1,22 @@
 package ex03;
 
-public class GugudanEx02 {
-    public static void main(String[] args) {
+class GugudanUtil {
+
+    static void gugudan(int x) {
         for (int i = 1; i <= 9; i++) {
-            for (int x = 2; x <= 9; x++) {
-                System.out.print(x + "*" + i + "=" + (x * i) + "\t");
-            }
-            System.out.println();
+            System.out.println(x + "*" + i + "=" + (x * i));
         }
+        System.out.println();
     }
 }
 
+
+
+public class GugudanEx04 {
+
+    public static void main(String[] args) {
+        // GugudanUtil 클래스에 gugudan 정적 메소드를 호출하시오.
+        // Parameter는 int 한개가 필요하다.
+        // 구구단을 출력해주는 메소드햐
+    }
+}
