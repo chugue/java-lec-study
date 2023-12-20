0303ccba92c0a1a316ce1493933d8b681cf50903
AI_Nomads <chugue85@gmail.com>
2023-12-20 11:37:35 +0900

    asedwe

---
+++ src/ex03/GugudanEx02.java
@@ -0,0 +1,15 @@
+package ex03;
+
+public class GugudanEx01 {
+    public static void main(String[] args) {
+
+        for (int j = 2; j < 10; j++) {
+            for (int i = 1; i < 10; i++) {
+                System.out.println(j + "*" + i + "=" + j * i);
+            }
+            System.out.println();
+        }
+    }
+}
+
+
