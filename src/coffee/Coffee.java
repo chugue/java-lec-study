d45bc1e3ad9822b5170900d690622cb09eb8512d
AI_Nomads <chugue85@gmail.com>
2024-01-05 12:14:32 +0900

    object copy

--- src/coffee/Coffee.java
+++ src/coffee/Coffee.java
@@ -25,31 +25,28 @@ public class Coffee {
                 System.out.println("잘못된 번호를 선택하셨습니다.");
         }
     }
-
-    public void americano (){
+    public void americano(){
         water_ml = 300;
         shots = 2;
         milk_ml = 0;
         status = "아메리카노 조리가 완료되었습니다";
     }
-    public void macchiato (){
+    public void macchiato(){
         water_ml = 100;
         shots = 2;
         milk_ml = 400;
         caramel_sp = 3;
         status = "마끼아또 조리가 완료되었습니다.";
-
     }
-    public void cafelatte (){
+    public void cafelatte(){
         water_ml = 100;
         shots = 2;
         milk_ml = 400;
         status = "카페라떼 조리가 완료되었습니다.";
     }
-    public void espresso (){
+    public void espresso(){
         water_ml = 100;
         shots = 2;
         status = "에스프레소 조리가 완료되었습니다.";
     }
-
 }
---
+++ src/ex14/example1/CopyEx02.java
@@ -0,0 +1,22 @@
+package ex14.example1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class CopyEx02 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1,2,3,4);
+
+        //map
+        List<Integer> newList = list.stream().map((i) -> i * 100).toList();
+
+        for (int i = 0; i < newList.size(); i++) {
+            System.out.println(newList.get(i));
+        }
+
+
+        //forEach
+        newList.stream().forEach(i -> System.out.println(i));
+    }
+}
